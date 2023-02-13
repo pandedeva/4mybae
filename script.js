c8fbb8f09dd988d8f776dae5e294bed027dbed58
deva <devapande66@gmail.com>
2023-02-14 02:16:53 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function popupku() {
+  var body = document.querySelector("body");
+  var pWm = document.createElement("a");
+  pWm.setAttribute("href", "https://www.instagram.com/k1llopps");
+  pWm.setAttribute("style", "text-decoration: none; color: white; opacity: .5; position: fixed; bottom : 10px; right: 20px");
+  pWm.innerHTML = "From yo handsome bf";
+  body.appendChild(pWm);
+}
